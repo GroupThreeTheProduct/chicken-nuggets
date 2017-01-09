5d2287d2c3546c1924c07d81946a6f2cd5424ae5
kennamz <kenna@zimfam.org>
2017-01-09 12:55:09 -0700

    started logo-pasting program

--- Kenna.py
+++ Kenna.py
@@ -1,31 +1,67 @@
-'''earthEyes demonstrates PIL.Image.paste()
-Unpublished work (c)2013 Project Lead The Way
-CSE Activity 1.3.7 PIL API
-Version 9/10/2013 '''
-
 import PIL
 import matplotlib.pyplot as plt
 import os.path              
 
-# Open the files in the same directory as the Python script
-directory = os.path.dirname(os.path.abspath(__file__))  
-student_file = os.path.join(directory, 'student.jpg')
+def pasteLogo(original_image):
+    """ 
+    Pastes logo in the corner of an image
+    """
+    im = Image.open(original_image)
+    im.paste('goat.png', (0,0))
+    
+    return result
 
-student_img = PIL.Image.open(student_file)
+def get_images(directory=None):
+    """ Returns PIL.Image objects for all the images in directory.
+    
+    If directory is not specified, uses current directory.
+    Returns a 2-tuple containing 
+    a list with a  PIL.Image object for each image file in root_directory, and
+    a list with a string filename for each image file in root_directory
+    """
+    
+    if directory == None:
+        directory = os.getcwd() # Use working directory if unspecified
+        
+    image_list = [] # Initialize aggregaotrs
+    file_list = []
+    
+    directory_list = os.listdir(directory) # Get list of files
+    for entry in directory_list:
+        absolute_filename = os.path.join(directory, entry)
+        try:
+            image = PIL.Image.open(absolute_filename)
+            file_list += [entry]
+            image_list += [image]
+        except IOError:
+            pass # do nothing with errors tying to open non-images
+    return image_list, file_list
+    
+def pasteLogoAll(directory=None):
+    """ 
+    Places a logo in the bottom right corner of each image in the directory
+    """
+    
+    if directory == None:
+        directory = os.getcwd() # Use working directory if unspecified
+        
+    # Create a new directory 'modified'
+    new_directory = os.path.join(directory, 'modified')
+    try:
+        os.mkdir(new_directory)
+    except OSError:
+        pass # if the directory already exists, proceed  
+    
+    #load all the images
+    image_list, file_list = get_images(directory)  
 
-# Open, resize, and display earth
-earth_file = os.path.join(directory, 'earth.png')
-earth_img = PIL.Image.open(earth_file)
-earth_small = earth_img.resize((89, 87)) #eye width and height measured in plt
-
-# Paste earth into right eye and display
-# Uses alpha from mask
-student_img.paste(earth_small, (1162, 966), mask=earth_small)
-student_img.paste(earth_small, (700, 940), mask=earth_small)
-# Display
-fig3, axes3 = plt.subplots(1, 2)
-axes3[0].imshow(student_img, interpolation='none')
-axes3[1].imshow(student_img, interpolation='none')
-axes3[1].set_xlim(500, 1500)
-axes3[1].set_ylim(1130, 850)
-fig3.show()
+    #go through the images and save modified versions
+    for n in range(len(image_list)):
+        # Parse the filename
+        filename, filetype = os.path.splitext(file_list[n])
+        
+        # Round the corners with radius = 30% of short side
+        new_image = round_corners(image_list[n])
+        #save the altered image, suing PNG to retain transparency
+        new_image_filename = os.path.join(new_directory, filename + '.png')
+        new_image.save(new_image_filename) 
